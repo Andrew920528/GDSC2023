595c8fd4ca4ab4f4bac50eb220b380638604038c
saum3 <saum3@gatech.edu>
2023-03-14 14:41:06 -0400

    added plant village page

--- Assets/Script/House/House.cs
+++ Assets/Script/House/House.cs
@@ -2,40 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class House
+public class House : MonoBehaviour
 {
-    [SerializeField] private string plantomoID = "";
-    [SerializeField] private string assetID = "";
-    [SerializeField] private bool isOwned = false;
-    [SerializeField] private int wateringPeriod = 0;//how often (in hours) it shoudl be watered
-    [SerializeField] private int watering = 0; //how many times it should be watered before owned
+    private string plantomoID = "";
+    private string assetID = "";
+    private bool isOwned = false;
+    private int wateringPeriod = 0;//how often (in hours) it shoudl be watered
+    private int watering = 0; //how many times it should be watered before owned
     //we'll have to design this
-    [SerializeField] private List<string> missions = new List<string>();
-
-    //making the object
-    public House(string plantomoID, int watering, int wateringPeriod) {
-        //still have to make this bigger
-        this.plantomoID = plantomoID;
-        this.watering = watering;
-        this.wateringPeriod = wateringPeriod;
-    }
-
-    //method for it to generate some sort of mission
-    public string generateMission()
-    {
-        string result = "";
-        Random rnd = new Random();
-        int num = rnd.Next(0, missions.length());
-        result = missions.GetEnumerator(num);
-        return result;
-    }
+    private List<string> missions = new List<string>();
 
     //check if it's owned depending on the wateringperiod
     public void checkIsOwned()
     {
         if(watering == 0)
         {
-            isOwned == true;
+            isOwned = true;
         } else
         {
             return;
