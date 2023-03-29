bf167fd272bba25dad8bbeea9460c53d64fa4893
robinhwhwk <88211687+robinhwhwk@users.noreply.github.com>
2023-03-28 23:05:03 -0400

    initialize firebase

---
+++ Assets/User.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User
+{
+    public string name;
+    public string email;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public User(string name, string email)
+    {
+        this.name = name;
+        this.email = email;
+    }
+}
